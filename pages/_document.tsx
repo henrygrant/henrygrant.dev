2001c8a2f8417d0ff6f4f11b5dba03cfd2b88722
Henry Grant <hgrant94@gmail.com>
2022-10-16 22:07:38 -0400

    add documentation (kind of), fix a typo in font import link tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorgin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet" /> 
       </Head>
       <body>
